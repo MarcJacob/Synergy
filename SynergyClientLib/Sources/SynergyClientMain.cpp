60d2fb33b5d87dd5fbbf2721062f9cde6e1deacf
MarcJacob <marcjacobpro@gmail.com>
2025-07-30 17:34:32 +0300

    Set up basic Client structure on how it is interacted with by the platform and render layer.

--- SynergyClientLib/Sources/SynergyClientMain.cpp
+++ SynergyClientLib/Sources/SynergyClientMain.cpp
@@ -4,8 +4,25 @@
 #include <iostream> // TODO instead of using iostream the user of the library should provide
 // contextual data so it can use any logging solution it wants.
 
+#define DLL_EXPORT extern "C" __declspec(dllexport)
+
 DLL_EXPORT void Hello()
 {
 	std::cout << "Hello World from Synergy Client Lib !" << "\n";
 	std::cout << "VERSION 0.1 - IN DEV\n";
+}
+
+DLL_EXPORT void StartClient(ClientContext& Context)
+{
+	std::cout << "Starting client.\n";
+}
+
+DLL_EXPORT void RunClientFrame(ClientContext& Context, ClientFrameData& FrameData)
+{
+	std::cout << "Running client frame." << FrameData.FrameNumber << "\n\tFrame Time = " << FrameData.FrameTime << "\n";
+}
+
+DLL_EXPORT void ShutdownClient(ClientContext& Context)
+{
+	std::cout << "Shutting down client.\n";
 }
