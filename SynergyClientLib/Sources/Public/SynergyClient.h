cccac2f20c15adb102edf09dcd3577813afc64c1
MarcJacob <marcjacobpro@gmail.com>
2025-07-30 17:34:33 +0300

    Began client draw call structure.

--- SynergyClientLib/Sources/Public/SynergyClient.h
+++ SynergyClientLib/Sources/Public/SynergyClient.h
@@ -5,6 +5,64 @@
 
 #include <stdint.h>
 
+union ColorRGBA
+{
+	struct
+	{
+		uint8_t r, g, b, a;
+	};
+
+	uint32_t full;
+};
+
+/*
+	Type of a draw call. Tells the render layer what kind of drawing to do, and what function to call to retrieve the correct
+	data structure from the draw call memory.
+*/
+enum DrawCallType
+{
+	INVALID, // Invalid draw call that wasn't correctly initialized from 0ed memory, or a signal that end of buffer was reached.
+	RECTANGLE, // Draw pixels with a corner origin and a specific width and height.
+	ELLIPSE, // Draw pixels in an ellipse with a specific origin and radius.
+	BITMAP,
+};
+
+// Base class for all draw call data classes. Contains spatial and visual transform information relevant to all types.
+struct DrawCall
+{
+	DrawCallType
+
+	// Origin coordinates of the draw call to be interpreted differently depending on type.
+	uint16_t x, y;
+
+	// Rotation in degrees of the drawn shape.
+	uint16_t angleDeg;
+
+	ColorRGBA color;
+};
+
+/*
+	Data for a Rectangle type draw call. Origin coordinates should be interpreted as top left corner position.
+	Angle should be interpreted as Rectangle Width along cosinus, Height along sinus at Angle = 0.
+*/
+struct RectangleDrawCallData : public DrawCall
+{
+	// Dimensions of rectangle.
+	uint16_t width, height;
+};
+
+/*
+	Contains all draw calls emitted by the client over a single frame.
+*/
+struct ClientFrameDrawCallBuffer
+{
+	/* 
+		Allocates room in the buffer to build a drawcall of the given type.
+		Returns non-null pointer to the place in memory to build the drawcall in if successful.
+	*/
+	void* BuildDrawCall(DrawCallType Type);
+};
+
 // Generic interface for a memory manager usable by the Client for Persistent and Frame memory.
 struct ClientMemoryManager
 {
@@ -16,7 +74,6 @@ struct ClientMemoryManager
 // Collection of platform functions that can be called from Client code.
 struct PlatformAPI
 {
-
 };
 
 // Persistent context data for a single execution of a client. Effectively acts as the Client's static memory.
@@ -42,8 +99,10 @@ struct ClientFrameData
 	size_t FrameNumber;
 	float FrameTime;
 
-	// Memory for this specific frame. Anything allocated here should be wiped automatically at the end of the frame by the platform.
+	// General-purpose Memory for this specific frame. Anything allocated here should be wiped automatically at the end of the frame by the platform.
 	ClientMemoryManager FrameMemory;
+
+	// 
 };
 
 // Contains function pointers associated with symbol names for easier symbol loading on the platform and to provide a centralized calling
