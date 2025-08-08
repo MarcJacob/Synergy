cb2e6a90feb6bb9c7e5524bda0e18f7101bc0cc0
JACOB Marc - Contractor <marc.jacob@external.thalesgroup.com>
2025-08-08 15:45:55 +0300

    Small change to SOURCE INC FILE prepro macro system so it expands to a message with the name of the translation unit.

--- SynergyCoreLib/Includes/Public/SynergyCore.h
+++ SynergyCoreLib/Includes/Public/SynergyCore.h
@@ -9,15 +9,22 @@
 // TRANSLATION UNIT & SOURCE INC FILE SYSTEM
 
 #ifndef TRANSLATION_UNIT
-#define ASSERT_RESULT 0
+#define INSIDE_TRANSLATION_UNIT 0
 #else
-#define ASSERT_RESULT 1
+#define INSIDE_TRANSLATION_UNIT 1
 #endif
+
+#define PRE_STR(str) #str
+#define STR(str) PRE_STR(str)
 
 // Files meant to be compiled as a translation unit should define a value for TRANSLATION_UNIT.
 
+#if INSIDE_TRANSLATION_UNIT
 // Used at the top of INC files, to streamline checking for a defined Translation Unit the INC file is part of.
-#define SOURCE_INC_FILE() static_assert(ASSERT_RESULT, "INC File " __FILE__ " must be included within a translation unit and NOT compiled on its own !");
-
+#define SOURCE_INC_FILE() static_assert(1, "This file is part of " STR(TRANSLATION_UNIT));
+#else
+// Used at the top of INC files, to streamline checking for a defined Translation Unit the INC file is part of.
+#define SOURCE_INC_FILE() static_assert(0, "INC File " __FILE__ " must be included within a translation unit and NOT compiled on its own !");
+#endif
 
 #endif // SYNERGY_CORE_INCLUDED
